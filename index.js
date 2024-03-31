5d39e3b6c064f13ff9b94ee49068ef619974084c
Szymon Kłos <eszkadev@gmail.com>
2024-03-31 16:33:12 +0200

    Filter messages, Polish messages
    Signed-off-by: Szymon Kłos <eszkadev@gmail.com>

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const DISCORD_FILE_LIMIT = 8000000;
 
 export default {
   async email(message, env, ctx) {
+    const allowList = [env.ALLOWED_ADDRESS];
+    if (allowList.indexOf(message.from) == -1 && allowList.indexOf(message.sender) == -1) {
+      message.setReject("Odrzucono: niedozwolony nadawca");
+      return;
+    }
     let rawEmail = new Response(message.raw);
     let arrayBuffer = await rawEmail.arrayBuffer();
     const parser = new PostalMime.default();
@@ -16,6 +21,11 @@ export default {
     if (!emailText) {
       // If there is no text, try to get the text from the html
       emailText = convert(email.html);
+    }
+    const forbiddenString = env.FORBIDDEN_STRING;
+    if (emailText.indexOf(forbiddenString) != -1) {
+      message.setReject("Odrzucono: niedozwolony ciąg znaków");
+      return;
     }
 // The overall limit is 6000 characters, and we limit the embed body to 4096 characters, so the rest has ~1900 characters to work with
     let embedBody = JSON.stringify({
@@ -26,14 +36,14 @@ export default {
           emailText.length > DISCORD_EMBED_LIMIT
               ? `${emailText.substring(
                   0,
-                  DISCORD_EMBED_LIMIT - 12
-                )}...(TRIMMED)`
+                  DISCORD_EMBED_LIMIT - 100
+                )}...(WIADOMOŚĆ JEST DŁUŻSZA, PROSZĘ SPRAWDZIĆ ZAŁĄCZNIK Z PEŁNĄ WIADOMOŚCIĄ)`
               : emailText,
           author: {
             name: `${(this.trimToLimit(email.from.name, 100))}${email.from.name.length > 64 ? "\n" : " "}<${this.trimToLimit(email.from.address, 100)}>`, // Limit of 256 characters, but we will be a bit careful
           },
           footer: {
-            text: `This email was sent to ${this.trimToLimit(message.to, 100)}\nEnvelope From: ${this.trimToLimit(message.from, 100)}`, // Limit of 2048 characters, but we will be careful
+            text: `Ten mail otrzymano od: ${this.trimToLimit(message.from, 100)}`, // Limit of 2048 characters, but we will be careful
           },
         },
       ],
@@ -46,12 +56,12 @@ export default {
       });
       // If the text is too big, we need truncate the blob.
       if (newTextBlob.size < DISCORD_FILE_LIMIT) {
-        formData.append("files[0]", newTextBlob, "email.txt");
+        formData.append("files[0]", newTextBlob, "wiadomosc.txt");
       } else {
         formData.append(
           "files[0]",
           newTextBlob.slice(0, DISCORD_FILE_LIMIT, "text/plain"),
-          "email-trimmed.txt"
+          "wiadomosc-obcieta.txt"
         );
       }
     }
@@ -70,9 +80,11 @@ export default {
     // Or you could make it fail if the webhook fails, causing the sending mail server to error out.
     // Or you could do something more complex with adding it to a Queue and retrying sending to Discord, etc
     // For now, I don't really care about those conditions
-    if (env.FORWARD_TO_ADDRESS) {
-      await message.forward(env.FORWARD_TO_ADDRESS);
-    }
+    try {
+      if (env.FORWARD_TO_ADDRESS) {
+        await message.forward(env.FORWARD_TO_ADDRESS);
+      }
+    } catch (e) {}
   },
   trimToLimit(input, limit) {
     return input.length > limit ? `${input.substring(0, limit - 12)}...(TRIMMED)` : input;
